16e8edcea7678362bc1e23eebe82a007b69237c7
Rohan Jha <rohan-jha@outlook.com>
2026-01-18 12:59:50 -0800

    Modify grade_calculator.py

--- python/grade_calculator.py
+++ python/grade_calculator.py
@@ -1,25 +1,19 @@
-category_list = []
-weights_list = []
-total_points_list = []
-earned_points_list = []
-categories = int(input("How many categories do you have: "))
-for i in range(categories):
-    total_points = float(input(f"Category {i+1}: Total points: "))
-    earned_points = float(input(f"Category {i+1}: Earned points: "))
-    weight = float(input("Category weight (%): "))
-    category_list.append((earned_points / total_points) * weight)
-    weights_list.append(weight)
-    total_points_list.append(total_points)
-    earned_points_list.append(earned_points)
+def desire_grade(grade, grade_needed, assignment_worth, assigment_weight, total_points, earned_points):
+    category_grade = (earned_points/total_points) * assignment_weight
+    grade -= category_grade
+    result = (((grade_needed - grade) / assignment_weight) * (total_points + assignment_worth)) - earned_points
+    round(result, 2)
 
 
+# def assigment_affect()   
+grade = float(input("What's your grade (%): "))
 grade_needed = float(input("Your desired grade (%): "))
 assignment_worth = float(input("Assignment points: "))
-category = int(input("Which category # (above): "))
-total_points = total_points_list[category - 1]
-earned_points = earned_points_list[category - 1]
-weight = weights_list[category - 1]
-category_list.remove(category_list[category - 1])
-category_list = sum(category_list)
-result = (((grade_needed - category_list) / weight) * (total_points + assignment_worth)) - earned_points
-print(round(result, 2))
+assignment_weight = float(input("Weight of assignment category (%): "))
+total_points = float(input("Total points in category: "))
+earned_points = float(input("Earned points in category: "))
+
+print(desire_grade(grade, grade_needed, assignment_worth, assignment_weight, total_points, earned_points))
+
+    
+ 
